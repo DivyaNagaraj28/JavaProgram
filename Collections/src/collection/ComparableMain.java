d6d72654d1c56e4a008c862233178afc97c739ed
DivyaNagaraj28 <44865429+DivyaNagaraj28@users.noreply.github.com>
2018-11-10 00:39:33 +0530

    Update ComparableMain.java

--- Collections/src/collection/ComparableMain.java
+++ Collections/src/collection/ComparableMain.java
@@ -8,11 +8,11 @@ public class ComparableMain {
 
 	public static void main(String[] args) {
 		ArrayList<SortComparable> s=new ArrayList<>();	
-		s.add(new SortComparable(1009,"divya",15000,"ece"));
+		s.add(new SortComparable(1009,"div",15000,"ece"));
 		s.add(new SortComparable(1004,"mohan",16000,"acc"));
 		s.add(new SortComparable(1007,"vivek",15000,"manage"));
 		s.add(new SortComparable(1002,"vicky",18000,"tech"));
-		s.add(new SortComparable(1008,"kanchu",15000,"recep"));
+		
 		Collections.sort(s);
 
 		for(SortComparable ec:s)
